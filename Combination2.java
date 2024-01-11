51b5169a1931ee929a4d3648acf5c9f3b98e580e
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-01-11 15:02:54 +0530

    Combination 2 recursion

---
+++ Combination2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Combination2 {
+    public static void combination2(boolean[] boxes, int ci, int ti, int Ib){
+        if (ci>ti) {
+            for(int i=0; i<boxes.length; i++){
+              if (boxes[i]) {
+                System.out.print("i");
+              }else{
+                System.out.print("-");
+              }
+              System.out.println();
+            }
+            return;
+        }
+        for(int b = Ib+1; b< boxes.length; b++){
+        if (boxes[b] == false) {
+            boxes[b] =  true;
+            combination2(boxes,ci+1,ti , b);
+            boxes[b] = false;
+        }
+        }
+    }
+ public static void main(String[] args) {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nboxes = Integer.parseInt(br.readLine());
+        int ritems = Integer.parseInt(br.readLine());
+        combination2(new int[nboxes],1, ritems, -1);
+ }   
+}
