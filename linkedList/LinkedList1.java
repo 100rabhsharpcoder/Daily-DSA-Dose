d0f99062b44c011759fddb27e096ba82b0d60d34
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-01-12 18:11:46 +0530

    addtion of nodes int linked list

--- linkedList/LinkedList1.java
+++ linkedList/LinkedList1.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import Backtracking.permutationOfString2;
+
 public class LinkedList1 {
     public class ListNode {
         public int val;
@@ -26,4 +28,37 @@ public class LinkedList1 {
             ptr = ptr.next;
         }
     }
+
+    public ListNode InsertAtBegening(ListNode head , int valToInsert){
+        // create node to insert 
+        ListNode newNode =  new ListNode(valToInsert);
+       //Assign the next of this mode to head 
+        newNode.next = head;
+        // point the head of the list to this new node
+        head =  newNode;
+
+        return head;
+    }
+    public ListNode insertAtEnd(ListNode head , int valToInsert){
+        ListNode newNode = new ListNode(valToInsert);
+        ListNode ptr =  head;
+        while (ptr.next !=null) {
+            ptr = ptr.next;
+        }
+        ptr.next = newNode;
+        return head;
+    }
+
+     public ListNode insertAtMiddle(ListNode head ,int poition, int valToInsert){
+        ListNode newNode = new ListNode(valToInsert);
+        ListNode ptr =  head;
+        for(int i=0; i< poition ; i++) {
+            ptr = ptr.next;
+        }
+        newNode = ptr.next;
+        ptr.next = newNode;
+
+        return head;
+    }
 }
+
