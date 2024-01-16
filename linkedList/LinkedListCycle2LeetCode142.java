7bfb6b754d5b4dcb45eb7f39833cecfeda56e267
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-01-16 12:54:16 +0530

    Leet code 876 linked list

--- linkedList/LinkedListCycle2LeetCode142.java
+++ linkedList/LinkedListCycle2LeetCode142.java
@@ -34,4 +34,33 @@ public class LinkedListCycle2LeetCode142 {
 
     return null;
  } 
+
+
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast =  head;
+
+        while(fast!=null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+            // if(fast == null){
+            
+            //     break;
+            // }
+        }
+    return slow;
+    }
+}
 }
