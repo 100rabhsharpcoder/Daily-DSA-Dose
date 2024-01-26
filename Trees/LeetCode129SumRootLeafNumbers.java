469dd1acb649c274f741801da209f898705d688c
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-01-26 20:41:52 +0530

    LeetCode 129. Sum Root to Leaf Numbers

---
+++ Trees/LeetCode129SumRootLeafNumbers.java
@@ -0,0 +1,36 @@
+public class LeetCode129SumRootLeafNumbers {
+    public static void main(String[] args) {
+        
+    }
+    public class TreeNode {
+        int val;
+       TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+           this.right = right;
+       }
+   }
+   int total;
+   public int sumNumber(TreeNode root){
+   total = 0;
+   helper(root, 0);
+   return total;
+   }
+
+   public void helper(TreeNode root, int sum){
+    if (root == null) {
+        return;
+    }
+    sum =  sum *10 + root.val;
+    if (root.left == null && root.right == null) {
+        total +=sum;
+    }
+
+    helper(root.left, sum);
+    helper(root.right, sum);
+   }
+}
