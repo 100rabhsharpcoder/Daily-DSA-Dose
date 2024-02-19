1a096d6272cc6f8f92352b538732fc899fc9c39b
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-02-19 19:03:21 +0530

    OOPS

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,15 +1,24 @@
 package OOPS;
+class Student {
+    int rno;
+    String name;
+    float marks;
+     Student(int rno, String name, float marks){
+         this.rno = rno;
+         this.name = name;
+         this.marks = marks;
+     }
+
+}
 
 public class Main {
-    class Student {
-        int rno;
-        String name;
-        Float marks;
-        
-
-    }
+   
     public static void main(String[] args) {
-         Student saurabh = new Student();
-         System.out.println(saurabh);
+         Student saurabh = new Student(131, "satnxsnc", 5645);
+        //  System.out.println(saurabh);
+         System.out.println(saurabh.rno);
+         System.out.println(saurabh.name);
+         System.out.println(saurabh.marks);
+
     }
 }
