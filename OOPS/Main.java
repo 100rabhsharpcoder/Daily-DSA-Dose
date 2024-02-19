02c6400fbbeb76d202aba9fef9115c7b9e9c358a
100rabhsharpcoder <saurabhgathade@gmail.com>
2024-02-19 18:17:12 +0530

    OOPS

---
+++ OOPS/Main.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class Main {
+    class Student {
+        int rno;
+        String name;
+        Float marks;
+        
+
+    }
+    public static void main(String[] args) {
+         Student saurabh = new Student();
+         System.out.println(saurabh);
+    }
+}
